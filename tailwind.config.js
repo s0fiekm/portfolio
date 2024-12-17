29dd0861a23b97fcbef6cdbc7ee7d35e60dba8ee
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-12-17 23:27:24 +0100

    første commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['"Helvetica Neue"', "sans-serif"],
+        secondary: ['"Noto Sans"', "serif"],
+      },
+      borderWidth: {
+        0.5: "0.5px",
+      },
+      colors: {
+        highlight: "#0400E0",
+      },
+    },
+  },
+  plugins: [],
+};
