d837d0ba4dc758df2290b0a7ce8171c3a18281a2
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2024-12-29 13:04:01 +0100

    try again

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}", "./public/**/*.html"],
+  content: ["./src/**/*.{astro,html,js,ts,tsx}", "./public/**/*.html"],
   theme: {
     extend: {
       backgroundImage: {
